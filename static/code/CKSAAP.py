5405472fb10544675523892a36d8c28244959a27
snehaiasri <110008402+snehaiasri@users.noreply.github.com>
2026-01-02 19:10:57 +0530

    Add files via upload

---
+++ static/code/CKSAAP.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#_*_coding:utf-8_*_
+
+import sys, os
+from . import checkFasta
+
+def CKSAAP(fastas, gap=5, order = 'ACDEFGHIKLMNPQRSTVWY'):
+	if gap < 0:
+		print('Error: the gap should be equal or greater than zero' + '\n\n')
+		return 0
+
+	if checkFasta.minSequenceLength(fastas) < gap+2:
+		print('Error: all the sequence length should be larger than the (gap value) + 2 = ' + str(gap+2) + '\n\n')
+		return 0
+
+	AA = order
+	encodings = []
+	aaPairs = []
+	for aa1 in AA:
+		for aa2 in AA:
+			aaPairs.append(aa1 + aa2)
+	header = ['#']
+	for g in range(gap+1):
+		for aa in aaPairs:
+			header.append(aa + '.gap' + str(g))
+	encodings.append(header)
+	for i in fastas:
+		name, sequence = i[0], i[1]
+		code = [name]
+		for g in range(gap+1):
+			myDict = {}
+			for pair in aaPairs:
+				myDict[pair] = 0
+			sum = 0
+			for index1 in range(len(sequence)):
+				index2 = index1 + g + 1
+				if index1 < len(sequence) and index2 < len(sequence) and sequence[index1] in AA and sequence[index2] in AA:
+					myDict[sequence[index1] + sequence[index2]] = myDict[sequence[index1] + sequence[index2]] + 1
+					sum = sum + 1
+			for pair in aaPairs:
+				code.append(myDict[pair] / sum)
+		encodings.append(code)
+	return encodings
---
+++ static/code/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#_*_coding:utf-8_*_
+
+__all__ = ['readFasta', 'checkFasta',
+		   'CKSAAP', 
+		   'CKSAAGP',
+		   'CTDT', 'CTDD',
+		   'CTriad',
+		   ]
